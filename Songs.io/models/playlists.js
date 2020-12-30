10875abe315bb47697e06130c5e57239d6d89867
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2020-12-30 17:57:26 +0800

    Added mongoose behavior

---
+++ Songs.io/models/playlists.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playlistSchema = new Schema({
+    title: {
+        type: String, 
+        required: true
+    },
+    description:{
+        type: String,
+        required: false
+    },
+    // loop songs from database for each (display songs inside database)
+    // manual display one by one
+    // song id and this id contains all the songs
+    songs:{
+        type: String,
+        required: true
+    } // id = 1
+
+})
