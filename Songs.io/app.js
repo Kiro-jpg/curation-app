5c65fbb37023092568f17ea715cac4941efd58de
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-02 14:51:20 +0800

    Updated routes

--- Songs.io/app.js
+++ Songs.io/app.js
@@ -3,14 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 
-
+// routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var albumRouter = require('./routes/album');
+var artistRouter = require('/routes/artist');
+var playlistRouter = require('./routes/playlist');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/album', albumRouter);
+app.use('/artist', artistRouter);
+app.use('/playlist', playlistRouter);
+
+
+// port
 const port = 3000;
-const dbURL = 'mongodb+srv://josh:<password>@groovy.x687l.mongodb.net/groovy-db?retryWrites=true&w=majority'
+
+// database
+const dbURL = 'mongodb+srv://josh:Test1234@groovy.x687l.mongodb.net/groovy-db?retryWrites=true&w=majority'
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})).catch((err) => console.log(err));
+
+
+// express
 var app = express();
 
 // view engine setup
@@ -25,9 +50,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/about', aboutRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -51,13 +74,9 @@ app.get('/', (req, res) => {
   res.send('HAHAHAHAHAHAHAHAHAAHAHAHAHAHAHAHA')
 })
 
-// playlist routes
-// album routes
-// artist routes
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+
 
 module.exports = app;
--- Songs.io/models/playlists.js
+++ Songs.io/models/playlists.js
@@ -13,6 +13,7 @@ const playlistSchema = new Schema({
     // loop songs from database for each (display songs inside database)
     // manual display one by one
     // song id and this id contains all the songs
-})
+}, {timestamps: true});
 
 const Playlist = mongoose.model('Playlist', playlistSchema);
+module.exports = Playlist;
