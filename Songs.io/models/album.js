d39a73ef82d48d5fd510f83bf4164a30aeaa8c49
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-04 21:28:48 +0800

    Added schema

--- Songs.io/models/album.js
+++ Songs.io/models/album.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const albumSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    image:{
+        type: String,
+        required: false
+    },
+    description:{
+        type: String,
+        required: false
+    }
+}, {timestamps: true});
+
+const Album = mongoose.model('Album', albumSchema);
+module.exports = Album;
