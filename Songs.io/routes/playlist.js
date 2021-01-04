379bc30b40590ab909af83e563c28ad72e2f2283
migsbogs <75259766+migsbogs@users.noreply.github.com>
2021-01-04 20:06:58 +0800

    updated playlist

--- Songs.io/routes/playlist.js
+++ Songs.io/routes/playlist.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const playlist = require('../models/playlists');
 
 /* GET home page. */
 router.get('/playlist', function (req, res, next) {
+  const playlist = new playlist({
+    title: 'modtakels',
+    description: 'tae',
+    image: 'ihi'
+  });
+
+  playlist.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
+
+
   res.render('playlist', {
     title: 'yes'
     
   });
 });
 
-module.exports = router;
+module.exports = router; 
+
