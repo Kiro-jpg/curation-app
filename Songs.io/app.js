7335dbdc207f891a827186464b5454a9db7059f0
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-05 14:31:34 +0800

    updated dependencies

--- Songs.io/app.js
+++ Songs.io/app.js
@@ -34,7 +34,7 @@ mongoose.connect(dbURL, {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
