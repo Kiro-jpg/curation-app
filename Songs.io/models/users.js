5245bb44451f3e425a1f8241c219612fbfa7e529
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-05 17:37:53 +0800

    Added user schema

--- Songs.io/models/users.js
+++ Songs.io/models/users.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    username:{
+        type: String,
+        required: false
+    },
+}, {timestamps: true});
+
+const User = mongoose.model('User', usersSchema);
+module.exports = User;
