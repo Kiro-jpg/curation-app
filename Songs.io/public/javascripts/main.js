3f5c9262058ccf0e0903a99e65cfc2fd2ff97e48
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-06 16:44:33 +0800

    Added about design

--- Songs.io/public/javascripts/main.js
+++ Songs.io/public/javascripts/main.js
@@ -1,33 +1,14 @@
-$(document).ready(function () {
-  var count = 0;
-  var blocks = $(".block");
 
-  var testEffect = setInterval(function () {
-    $(blocks[count - 1]).removeClass("hover");
-    $(blocks[count]).addClass("hover");
-    count++;
-    if (count > 4) clearInterval(testEffect);
-  }, 800);
+
+
+$(document).ready(function() {
+  // Transition effect for navbar 
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 10) { 
+        $('.navbar').addClass('solid');
+    } else {
+        $('.navbar').removeClass('solid');
+    }
+  });
 });
-
-const target = {
-  clicked: 0,
-  currentFollowers: 90,
-  btn: document.querySelector("a.btn"),
-  fw: document.querySelector("span.followers")
-};
-
-const follow = () => {
-  target.clicked += 1;
-  target.btn.innerHTML = 'Following <i class="fas fa-user-times"></i>';
-
-  if (target.clicked % 2 === 0) {
-    target.currentFollowers -= 1;
-    target.btn.innerHTML = 'Follow <i class="fas fa-user-plus"></i>';
-  } else {
-    target.currentFollowers += 1;
-  }
-
-  target.fw.textContent = target.currentFollowers;
-  target.btn.classList.toggle("following");
-}
