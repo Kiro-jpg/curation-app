a44a5d544411a72f136e01064beb1f1a48e740a8
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-01-22 19:23:30 +0800

    Added song logic

--- Songs.io/controllers/index.js
+++ Songs.io/controllers/index.js
@@ -3,7 +3,6 @@ const User = require('../models/users');
 
 // render index page
 exports.render = function (req, res, next) {
-    console.log(req.body);
     User.find()
         .then((result) => {
             res.render('index.ejs', {
--- Songs.io/controllers/login.js
+++ Songs.io/controllers/login.js
@@ -2,20 +2,24 @@ var passport = require("passport");
 
 
 
-exports.render_login = function(req, res) 
- {
-     res.render('login', {title: 'Login'});
- }
-
-exports.login = function (req, res, next) {
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true
-    })(req, res, next);
+exports.render_login = function (req, res) {
+    res.render('login', {
+        title: 'Login'
+    });
 }
 
-exports.logout = function(req, res) {
+exports.login = function (req, res, next) {
+    console.log('im in');
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })(req, res, next);
+
+
+}
+
+exports.logout = function (req, res) {
     req.logout();
     req.flash('success_msg', 'You are logged out');
     res.redirect('/login');
