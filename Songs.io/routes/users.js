90ec71d0bbd15b4dbb5504a72a430248d20d5668
navarrajosh <73941578+navarrajosh@users.noreply.github.com>
2021-01-30 23:22:28 +0800

    added update function

--- Songs.io/routes/users.js
+++ Songs.io/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/users');
+const methodoverride = require("method-override");
+
+router.use(methodoverride("_method", {
+  methods: ["POST", "GET"]
+}));
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
@@ -25,5 +30,39 @@ router.get('/users/:id', function (req, res) {
     })
 });
 
+router.get('/users/:id', function (req, res, next) {
+  let userId = req.params.id;
+  let param = "/users/" + userId;
+  User.findById(userId)
+    .then(user => {
+      res.redirect(param, {
+        user: user
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
+router.put('/users/:id', function (req, res, next) {
+  let userId = req.params.id;
+  let param = "/users/" + userId;
+  userParams = {
+    name: req.body.username,
+    bio: req.body.userdescription,
+    image: req.body.userimagelink
+  };
+  console.log(userParams);
+  User.findByIdAndUpdate(userId, {
+      $set: userParams
+    })
+    .then(user => {
+      res.redirect(param);
+      res.locals.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 module.exports = router;
