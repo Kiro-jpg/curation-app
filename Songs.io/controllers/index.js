2cfd2cf2ed2834c7f2763493ccaa3947a6cf1d36
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-02-08 00:07:44 +0800

    Added dynamic data

--- Songs.io/controllers/index.js
+++ Songs.io/controllers/index.js
@@ -4,27 +4,14 @@ const User = require('../models/users');
 // render index page
 exports.render = function (req, res, next) {
 
-    var id;
-    switch (req.session.email) {
-        case "jm@gmail.com":
-            id = "5ffb23fd4e8e211dcc877aa9"
-            break;
-        case "mid23@gmail.com":
-            id = "6012bf5840f2b637704859b4"
-            break;
-        case "kuragure@gmail.com":
-            id = "6013c777c17fdd0d18584151"
-            break;
-    }
-
     if (req.session.email == "admin@gmail.com") {
         res.render("admin.ejs", {
             title: 'Groovy Admin'
         });
     } else {
-        // console.log(req.session.email);
-        // console.log(id);
-        User.findById(id)
+        User.findOne({
+                email: req.session.email
+            })
             .then((result) => {
                 res.render('index.ejs', {
                     title: 'Groovy',
--- Songs.io/controllers/register.js
+++ Songs.io/controllers/register.js
@@ -2,43 +2,58 @@ var User = require('../models/users');
 var bcrypt = require('bcryptjs');
 
 
-exports.render_register = function(req, res){
-    res.render('register.ejs', {
-      title: "Register",
-    });
+exports.render_register = function (req, res) {
+  res.render('register.ejs', {
+    title: "Register",
+  });
 }
 
-exports.do_register = function(req, res){
-    const {name, email, password, password2, username } = req.body;
-    let errors = [];
-    
-    // Check reqeuired fields
-    if(!name || !email || !password || !password2 || !username){
-      errors.push({msg: 'Please fill up all fields'});
-    }
+exports.do_register = function (req, res) {
+  const {
+    name,
+    email,
+    password,
+    password2,
+    username,
+    image
+  } = req.body;
+  let errors = [];
 
-    // Check password match
-    if(password !== password2){
-      errors.push({msg: 'Passwords do not match'});
-    }
+  // Check reqeuired fields
+  if (!name || !email || !password || !password2 || !username) {
+    errors.push({
+      msg: 'Please fill up all fields'
+    });
+  }
 
-    if(errors.length > 0){
-      res.render('register.ejs', {
-        title: "Register",
-        errors,
-        name,
-        email,
-        password,
-        username,
-        password2
-      });
-    } else {
-      User.findOne({email:email})
+  // Check password match
+  if (password !== password2) {
+    errors.push({
+      msg: 'Passwords do not match'
+    });
+  }
+
+  if (errors.length > 0) {
+    res.render('register.ejs', {
+      title: "Register",
+      errors,
+      name,
+      email,
+      password,
+      username,
+      password2
+    });
+  } else {
+    User.findOne({
+        email: email
+      })
       .then(user => {
-        if(user){
+        if (user) {
 
           //User exist
-          errors.push({ msg: 'Email is already registered'});
+          errors.push({
+            msg: 'Email is already registered'
+          });
           res.render('register', {
             errors,
             name,
@@ -48,33 +63,34 @@ exports.do_register = function(req, res){
             password2
 
           });
-        }else {
+        } else {
 
           const newUser = new User({
             name,
             email,
             username,
-            password
+            password,
+            image
           });
           // Hashing
 
           bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err,hash) =>{
-              if(err) throw err;
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
               // Set password to hash
               newUser.password = hash;
               //Save user
               newUser.save()
-              .then(user =>{
-                req.flash('success_msg', 'You are now registered and can log in');
-                res.redirect('/login');
-              })
-              .catch(err => console.log(err));
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/login');
+                })
+                .catch(err => console.log(err));
+            });
           });
-        });
 
-    }
-      
-    });
-    }
+        }
+
+      });
+  }
 }
